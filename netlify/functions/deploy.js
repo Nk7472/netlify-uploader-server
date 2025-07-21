33454e0fd407fa72a3fdaf982c7f4dd48a8b87fb
Nk7472 <177421309+Nk7472@users.noreply.github.com>
2025-07-22 02:36:25 +0530

    Create deploy.js

---
+++ netlify/functions/deploy.js
@@ -0,0 +1,36 @@
+const { exec } = require("child_process");
+
+exports.handler = async (event) => {
+  if (event.httpMethod === "OPTIONS") {
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "POST, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type",
+      },
+      body: "",
+    };
+  }
+
+  try {
+    // You can use your Netlify token and API inside here to upload files
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ message: "Upload triggered successfully" }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
+};
